83bd86d35e3a34283e9117393079061967ffd7eb
RomanDonw <romandonw628@gmail.com>
2026-08-30 16:27:43 +1000

    tried to fix stupid atomic int32 header-only implementation

--- include/libnthread/atomicimpl/int32.h
+++ include/libnthread/atomicimpl/int32.h
@@ -20,13 +20,13 @@
     typedef volatile int32_t NThreadAtomicInt32;
     #define NTHREAD_ATOMICINT32INIT(value) ((int32_t)(value))
 
-    static inline bool nthread_atomicint32_load(NThreadAtomicInt32 *variable)
+    static inline int32_t nthread_atomicint32_load(NThreadAtomicInt32 *variable)
     {
         #ifdef LIBNTHREAD_USEDMSVCONARM
             int32_t ret = __iso_volatile_load32(variable);
             __dmb(LIBNTHREAD_ARMMEMORYBARRIER);
         #else
-            bool ret = *(volatile int32_t *)variable;
+            int32_t ret = *(volatile int32_t *)variable;
             _ReadWriteBarrier();
         #endif
         return ret;
@@ -42,16 +42,16 @@
         #endif
     }
 
-    static inline int32_t nthread_atomic32_cmpxchgv(NThreadAtomicInt32 *variable, int32_t expected, int32_t desired)
-    { return _InterlockedCompareExchange32(variable, desired, expected); }
+    static inline int32_t nthread_atomicint32_cmpxchgv(NThreadAtomicInt32 *variable, int32_t expected, int32_t desired)
+    { return _InterlockedCompareExchange(variable, desired, expected); }
 
-    static inline int32_t nthread_atomicbool_cmpxchgt(NThreadAtomicInt32 *variable, int32_t expected, int32_t desired)
-    { return nthread_atomicbool_cmpxchgv(variable, expected, desired) == expected; }
+    static inline int32_t nthread_atomicint32_cmpxchgt(NThreadAtomicInt32 *variable, int32_t expected, int32_t desired)
+    { return nthread_atomicint32_cmpxchgv(variable, expected, desired) == expected; }
 
-    static inline int32_t nthread_atomicbool_cmpxchgp(NThreadAtomicInt32 *variable, bool *expected, bool desired)
+    static inline int32_t nthread_atomicint32_cmpxchgp(NThreadAtomicInt32 *variable, int32_t *expected, int32_t desired)
     {
         register int32_t expval = *expected;
-        register int32_t oldvarval = nthread_atomicbool_cmpxchgv(variable, expval, desired);
+        register int32_t oldvarval = nthread_atomicint32_cmpxchgv(variable, expval, desired);
         if (oldvarval == expval) return true;
         *expected = oldvarval;
         return false;
@@ -59,26 +59,26 @@
 
 #elif defined(LIBNTHREAD_USEGCCORCLANGATOMICS)
 
-    typedef bool NThreadAtomicBool;
-    #define NTHREAD_ATOMICBOOLINIT(value) ((bool)(value))
+    typedef int32_t NThreadAtomicInt32;
+    #define NTHREAD_ATOMICINT32INIT(value) ((int32_t)(value))
 
-    static inline bool nthread_atomicbool_load(NThreadAtomicBool *variable) { return __atomic_load_n(variable, __ATOMIC_SEQ_CST); }
-    static inline void nthread_atomicbool_store(NThreadAtomicBool *variable, bool desired) { __atomic_store_n(variable, desired, __ATOMIC_SEQ_CST); }
+    static inline int32_t nthread_atomicint32_load(NThreadAtomicInt32 *variable) { return __atomic_load_n(variable, __ATOMIC_SEQ_CST); }
+    static inline void nthread_atomicint32_store(NThreadAtomicInt32 *variable, int32_t desired) { __atomic_store_n(variable, desired, __ATOMIC_SEQ_CST); }
 
-    static inline bool nthread_atomicbool_cmpxchgv(NThreadAtomicBool *variable, bool expected, bool desired)
+    static inline int32_t nthread_atomicint32_cmpxchgv(NThreadAtomicBool *variable, int32_t expected, int32_t desired)
     {
-        bool exponstackcopy = expected;
+        int32_t exponstackcopy = expected;
         __atomic_compare_exchange_n(variable, &exponstackcopy, desired, false, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);
         return exponstackcopy;
     }
 
-    static inline bool nthread_atomicbool_cmpxchgt(NThreadAtomicBool *variable, bool expected, bool desired)
+    static inline int32_t nthread_atomicint32_cmpxchgt(NThreadAtomicBool *variable, int32_t expected, int32_t desired)
     {
-        bool exponstackcopy = expected;
+        int32_t exponstackcopy = expected;
         return __atomic_compare_exchange_n(variable, &exponstackcopy, desired, false, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);
     }
 
-    static inline bool nthread_atomicbool_cmpxchgp(NThreadAtomicBool *variable, bool *expected, bool desired)
+    static inline int32_t nthread_atomicint32_cmpxchgp(NThreadAtomicBool *variable, int32_t *expected, int32_t desired)
     { return __atomic_compare_exchange_n(variable, expected, desired, false, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST); }
 
 #endif
